f0dd8b06956b273440e6c9a2742076f088a92537
maishurong <maishurong@coding.net>
2021-07-04 18:05:29 +0800

    first commit

---
+++ src/linked-list.ts
@@ -0,0 +1,156 @@
+
+/**
+ * @description 链表
+ * @link https://github.com/trekhleb/javascript-algorithms/blob/master/src/data-structures/linked-list/README.zh-CN.md
+ */
+
+interface LinkedNode<T = unknown> {
+    prev: LinkedNode<T> | null;
+    next: LinkedNode<T> | null;
+    data: T
+}
+
+const createNode = <T>(v: T): LinkedNode<T> => ({
+    prev: null,
+    next: null,
+    data: v,
+});
+
+export default class LinkedList<T = unknown> {
+    head: LinkedNode<T> | null = null;
+    tail: LinkedNode<T> | null = null;
+
+    constructor(...values: T[]) {
+        if (typeof values === 'undefined') return;
+
+        const length = values.length;
+        let lastNode: LinkedNode<T>;
+
+        values.forEach((v, i) => {
+            const currentNode = createNode(v);
+
+            if (lastNode) {
+                lastNode.next = currentNode;
+                currentNode.prev = lastNode;
+            } else {
+                lastNode = currentNode;
+                this.head = lastNode;
+            }
+
+            // last one
+            if (i === length - 1) {
+                this.tail = currentNode;
+            }
+        });
+    }
+
+    add(value: T) {
+        const targetNode = createNode(value);
+
+        // list is empty
+        if (this.head === null) {
+            this.head = targetNode;
+            this.tail = targetNode;
+            return;
+        }
+
+        if (this.tail) {
+            this.tail.next = targetNode;
+            targetNode.prev = this.tail;
+            this.tail = targetNode;
+        }
+    }
+
+    prepend(value: T) {
+        const targetNode = createNode(value);
+
+        if (this.head === null) {
+            this.head = targetNode;
+            this.tail = targetNode;
+            return;
+        }
+
+        this.head.prev = targetNode;
+        targetNode.next = this.head;
+        this.head = targetNode;
+    }
+
+    contains(value: T): boolean {
+        let foundNode = this.head;
+
+        if (this.head === null) return false;
+
+        while (foundNode && foundNode.data !== value) {
+            foundNode = foundNode.next;
+        }
+
+        return !!foundNode;
+    }
+
+    remove(value: T): boolean {
+        let foundNode = this.head;
+
+        if (this.head === null) return false;
+
+        while (foundNode && foundNode.data !== value) {
+            foundNode = foundNode.next;
+        }
+
+        if (!foundNode) return false;
+
+        // single node
+        if (foundNode.prev === null && foundNode.next === null) {
+            this.head = null;
+            this.tail = null;
+            return true;
+        }
+
+        // head
+        if (foundNode.prev === null && foundNode.next) {
+            foundNode.next.prev = null;
+            this.head = foundNode.next;
+            return true;
+        }
+
+        // tail
+        if (foundNode.next === null && foundNode.prev) {
+            foundNode.prev.next = null;
+            foundNode.prev = null;
+            this.tail = foundNode.prev;
+            return true;
+        }
+
+        // middle
+        if (foundNode.prev && foundNode.next) {
+            foundNode.prev.next = foundNode.next;
+            foundNode.next.prev = foundNode.prev;
+            foundNode.prev = null;
+            foundNode.next = null;
+            return true;
+        }
+
+        return false;
+    }
+
+    traverse(callback: (value: T) => void) {
+        let lastNode = this.head;
+
+        if (this.head === null) return;
+
+        while (lastNode) {
+            callback(lastNode.data);
+            lastNode = lastNode.next;
+        }
+    }
+
+    teverseTraversal(callback: (value: T) => void) {
+        let lastNode = this.tail;
+
+        if (this.tail === null) return;
+
+        while (lastNode) {
+            callback(lastNode.data);
+            lastNode = lastNode.prev;
+        }
+    }
+}
