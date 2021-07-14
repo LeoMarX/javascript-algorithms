0cd2c52c9381f76a7742ee0481654a8294a4f360
maishurong <maishurong@coding.net>
2021-07-15 07:49:14 +0800

    chore: add husky hook

--- src/stack/stack.ts
+++ src/stack/stack.ts
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * @description 栈
  * @link https://github.com/trekhleb/javascript-algorithms/tree/master/src/data-structures/stack
  * @export
@@ -7,36 +7,35 @@
  */
 import { LinkedList } from '../linked-list/linked-list';
 
-
 export class Stack<T> {
-    linkedList: LinkedList<T>;
+  linkedList: LinkedList<T>;
 
-    constructor() {
-        this.linkedList = new LinkedList();
-    }
+  constructor() {
+    this.linkedList = new LinkedList();
+  }
 
-    push(value: T): void {
-        this.linkedList.prepend(value);
-    }
+  push(value: T): void {
+    this.linkedList.prepend(value);
+  }
 
-    pop():T | null {
-        const deleteNode = this.linkedList.deleteHead();
-        return deleteNode?.value || null;
-    }
+  pop(): T | null {
+    const deleteNode = this.linkedList.deleteHead();
+    return deleteNode?.value || null;
+  }
 
-    peek():T | null {
-        return this.linkedList.head?.value || null;
-    }
+  peek(): T | null {
+    return this.linkedList.head?.value || null;
+  }
 
-    isEmpty(): boolean {
-        return this.linkedList.head === null;
-    }
+  isEmpty(): boolean {
+    return this.linkedList.head === null;
+  }
 
-    toArray(): T[] {
-        return this.linkedList.toArray().map(n => n.value);
-    }
+  toArray(): T[] {
+    return this.linkedList.toArray().map((n) => n.value);
+  }
 
-    toString(callback?: (v: T) => string): string {
-        return this.linkedList.toString(callback);
-    }
+  toString(callback?: (v: T) => string): string {
+    return this.linkedList.toString(callback);
+  }
 }
