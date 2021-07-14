4d47168f52774e59e4163dad1da34c76da25e665
maishurong <maishurong@coding.net>
2021-07-14 19:48:33 +0800

    feat: add stack

---
+++ src/stack/stack.ts
@@ -0,0 +1,42 @@
+/**
+ * 
+ * @description 栈
+ * @link https://github.com/trekhleb/javascript-algorithms/tree/master/src/data-structures/stack
+ * @export
+ * @class Stack
+ */
+import { LinkedList } from '../linked-list/linked-list';
+
+
+export class Stack<T> {
+    linkedList: LinkedList<T>;
+
+    constructor() {
+        this.linkedList = new LinkedList();
+    }
+
+    push(value: T): void {
+        this.linkedList.prepend(value);
+    }
+
+    pop():T | null {
+        const deleteNode = this.linkedList.deleteHead();
+        return deleteNode?.value || null;
+    }
+
+    peek():T | null {
+        return this.linkedList.head?.value || null;
+    }
+
+    isEmpty(): boolean {
+        return this.linkedList.head === null;
+    }
+
+    toArray(): T[] {
+        return this.linkedList.toArray().map(n => n.value);
+    }
+
+    toString(callback?: (v: T) => string): string {
+        return this.linkedList.toString(callback);
+    }
+}
