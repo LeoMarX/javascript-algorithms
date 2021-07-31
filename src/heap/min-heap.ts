a538fd6882a15646cc51ce26be1db5564711f7b8
maishurong <maishurong@coding.net>
2021-07-31 15:43:30 +0800

    feat: add minheap first version

--- src/heap/min-heap.ts
+++ src/heap/min-heap.ts
@@ -1,58 +1,95 @@
-class TreeNode<T> {
-    left: NullableNode<T>;
-    right: NullableNode<T>;
-    value: T;
-
-    constructor(value: T, leftNode?: TreeNode<T>, rightNode?: TreeNode<T>) {
-        this.value = value;
-        this.left = leftNode ?? null;
-        this.right = rightNode ?? null;
-    }
-}
-
-type NullableNode<T> = TreeNode<T> | null;
-
 /**
  * 
  * @description 最小堆
+ * @note 若一个节点的索引是 n，那么它的左子树的索引是 2n+1，右子树的索引是 2n+2
  * @link https://github.com/trekhleb/javascript-algorithms/tree/master/src/data-structures/heap
  * @link https://blog.bitsrc.io/implementing-heaps-in-javascript-c3fbf1cb2e65
  * @export
  * @class MinHeap
  * @template T
  */
-export class MinHeap<T> {
-    private root: NullableNode<T>;
+export class MinHeap<T = number> {
+    private heap: T[];
 
     constructor() {
-        this.root = null;
+        this.heap = [];
     }
 
     add(value: T): this {
-        if (!this.root) {
-            this.root = new TreeNode(value);
+        const heap = this.heap;
+        heap.push(value);
+
+        let current = heap.length - 1;
+        let parent = Math.floor((current - 1) / 2);
+
+        while (heap[current] && heap[parent] && heap[current] < heap[parent]) {
+            [heap[current], heap[parent]] = [heap[parent], heap[current]];
+            current = parent;
+            parent = Math.floor((current - 1) / 2);
         }
-        
+
         return this;
     }
 
-    remove(value: T) {
+    remove(value: T, comparator?: (v1: T, v2: T) => 0 | -1 | 1): this {
+        const heap = this.heap;
+        const compareFn = comparator ?? ((v1: unknown, v2: unknown) => (v1 as number) - (v2 as number));
 
+        let current = heap.length -1;
+        let parent = Math.floor((current - 1) / 2);
+
+        while (heap[current] && heap[parent] && value >= heap[current]) {
+            current = parent;
+            parent = Math.floor((current - 1) / 2);
+        }
+
+        for (let i = current; i >= parent; i --) {
+            if (compareFn(value, heap[i]) === 0) {
+                heap.slice(i, 1);
+                return this;
+            }
+        }
+
+        return this;
     }
 
-    poll() {
+    find(value: T, comparator?: (v1: T, v2: T) => 0 | -1 | 1): number[] {
+        const foundIndex: number[] = [];
 
+        const heap = this.heap;
+        const compareFn = comparator ?? ((v1: unknown, v2: unknown) => (v1 as number) - (v2 as number));
+
+        let current = heap.length -1;
+        let parent = Math.floor((current - 1) / 2);
+
+        while (heap[current] && heap[parent] && value >= heap[current]) {
+            current = parent;
+            parent = Math.floor((current - 1) / 2);
+        }
+
+        for (let i = current; i >= parent; i --) {
+            if (compareFn(value, heap[i]) === 0) {
+                heap.slice(i, 1);
+                foundIndex.push(i);
+            }
+        }
+
+        return foundIndex;
     }
 
-    peek() {
-
+    poll(): T {
+        return this.heap.slice(0, 1)[0];
     }
 
-    isEmpty() {
-
+    peek(): T | null {
+        return this.heap[0] ?? null;
     }
 
-    toString() {
+    isEmpty(): boolean {
+        return this.heap.length === 0;
+    }
 
+    toString(): string {
+        return this.heap.toString();
     }
 }
