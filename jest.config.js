2d5834844e7263b1f09ccee3d9faeaf177371497
maishurong <maishurong@coding.net>
2021-08-22 15:49:17 +0800

    chore: add debugger for jest

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'ts-jest/presets/js-with-ts-esm',
   // testEnvironment: 'node',
   extensionsToTreatAsEsm: ['.ts'],
+  maxConcurrency: 8,
   globals: {
     'ts-jest': {
       useESM: true,
