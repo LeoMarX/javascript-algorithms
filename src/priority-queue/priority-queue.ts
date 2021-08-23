ad92841a5e7b9df1882716193e862f304b1c1273
maishurong <maishurong@coding.net>
2021-08-23 11:41:50 +0800

    feat: add PriorityQueue

---
+++ src/priority-queue/priority-queue.ts
@@ -0,0 +1,34 @@
+/**
+ *
+ * @description 优先队列
+ * 
+ * @export
+ * @class PriorityQueue
+*/
+
+
+export class PriorityQueue<T = number> {
+    constructor() {
+
+    }
+
+    add(value: T, priority: number) {
+
+    }
+
+    poll() {
+
+    }
+
+    peek() {
+
+    }
+
+    hasValue(value: T) {
+
+    }
+
+    changePriority(value: T, priority: number) {
+
+    }
+}
